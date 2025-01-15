03d7a9f5355c1727ce5b6523806fc4c0394b290d
inotia00 <108592928+inotia00@users.noreply.github.com>
2025-01-15 21:09:00 +0900

    feat: Remove unnecessary Constants (Match with uDrop)

--- patches/src/main/kotlin/app/revanced/patches/shared/gms/GmsCoreSupportPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/shared/gms/GmsCoreSupportPatch.kt
@@ -326,84 +326,33 @@ fun gmsCoreSupportPatch(
  * that are present in GmsCore which need to be transformed.
  */
 private object Constants {
-    /**
-     * All permissions.
-     */
     val PERMISSIONS = setOf(
         // C2DM / GCM
         "com.google.android.c2dm.permission.RECEIVE",
         "com.google.android.c2dm.permission.SEND",
-        "com.google.android.gtalkservice.permission.GTALK_SERVICE",
         "com.google.android.providers.gsf.permission.READ_GSERVICES",
-
-        // GAuth
-        "com.google.android.googleapps.permission.GOOGLE_AUTH",
-        "com.google.android.googleapps.permission.GOOGLE_AUTH.cp",
-        "com.google.android.googleapps.permission.GOOGLE_AUTH.local",
-        "com.google.android.googleapps.permission.GOOGLE_AUTH.mail",
-        "com.google.android.googleapps.permission.GOOGLE_AUTH.writely",
-
-        // Ad
-        "com.google.android.gms.permission.AD_ID_NOTIFICATION",
-        "com.google.android.gms.permission.AD_ID",
     )
 
-    /**
-     * All intent actions.
-     */
     val ACTIONS = setOf(
-        // location
-        "com.google.android.gms.location.places.ui.PICK_PLACE",
-        "com.google.android.gms.location.places.GeoDataApi",
-        "com.google.android.gms.location.places.PlacesApi",
-        "com.google.android.gms.location.places.PlaceDetectionApi",
-        "com.google.android.gms.wearable.MESSAGE_RECEIVED",
-        "com.google.android.gms.checkin.BIND_TO_SERVICE",
-
         // C2DM / GCM
         "com.google.android.c2dm.intent.REGISTER",
         "com.google.android.c2dm.intent.REGISTRATION",
-        "com.google.android.c2dm.intent.UNREGISTER",
         "com.google.android.c2dm.intent.RECEIVE",
         "com.google.iid.TOKEN_REQUEST",
-        "com.google.android.gcm.intent.SEND",
-
-        // car
-        "com.google.android.gms.car.service.START",
 
         // people
         "com.google.android.gms.people.service.START",
 
-        // wearable
-        "com.google.android.gms.wearable.BIND",
-
         // auth
         "com.google.android.gsf.login",
         "com.google.android.gsf.action.GET_GLS",
-        "com.google.android.gms.common.account.CHOOSE_ACCOUNT",
-        "com.google.android.gms.auth.login.LOGIN",
-        "com.google.android.gms.auth.api.credentials.PICKER",
-        "com.google.android.gms.auth.api.credentials.service.START",
         "com.google.android.gms.auth.service.START",
-        "com.google.firebase.auth.api.gms.service.START",
-        "com.google.android.gms.auth.be.appcert.AppCertService",
-        "com.google.android.gms.credential.manager.service.firstparty.START",
-        "com.google.android.gms.auth.GOOGLE_SIGN_IN",
         "com.google.android.gms.signin.service.START",
-        "com.google.android.gms.auth.api.signin.service.START",
-        "com.google.android.gms.auth.api.identity.service.signin.START",
         "com.google.android.gms.accountsettings.action.VIEW_SETTINGS",
-
-        // fido
-        "com.google.android.gms.fido.fido2.privileged.START",
+        "com.google.android.gms.auth.account.authapi.START",
 
         // gass
         "com.google.android.gms.gass.START",
-
-        // games
-        "com.google.android.gms.games.service.START",
-        "com.google.android.gms.games.PLAY_GAMES_UPGRADE",
-        "com.google.android.gms.games.internal.connect.service.START",
 
         // help
         "com.google.android.gms.googlehelp.service.GoogleHelpService.START",
@@ -420,35 +369,24 @@ private object Constants {
         // phenotype
         "com.google.android.gms.phenotype.service.START",
 
-        // location
-        "com.google.android.gms.location.reporting.service.START",
-
         // misc
-        "com.google.android.gms.gmscompliance.service.START",
-        "com.google.android.gms.oss.licenses.service.START",
-        "com.google.android.gms.tapandpay.service.BIND",
-        "com.google.android.gms.measurement.START",
-        "com.google.android.gms.languageprofile.service.START",
         "com.google.android.gms.clearcut.service.START",
-        "com.google.android.gms.icing.LIGHTWEIGHT_INDEX_SERVICE",
-        "com.google.android.gms.icing.INDEX_SERVICE",
-        "com.google.android.gms.mdm.services.START",
+        "com.google.android.gms.languageprofile.service.START",
+        "com.google.android.gms.measurement.START",
+        "com.google.android.gms.pseudonymous.service.START",
+        "com.google.android.gms.usagereporting.service.START",
+        "com.google.android.gms.wallet.service.BIND",
 
         // potoken
         "com.google.android.gms.potokens.service.START",
 
-        // droidguard, safetynet
+        // droidguard
         "com.google.android.gms.droidguard.service.START",
-        "com.google.android.gms.safetynet.service.START",
     )
 
-    /**
-     * All content provider authorities.
-     */
     val AUTHORITIES = setOf(
         // gsf
         "com.google.android.gsf.gservices",
-        "com.google.settings",
 
         // auth
         "com.google.android.gms.auth.accounts",
